891652823b046aa6bf51a6996ce4860e559cd45d
Sergii Kauk <sergii@kauk.at>
2025-04-28 14:21:37 +0200

    Draft version of the plugin

--- test/integration/IntegrationTest.php
+++ test/integration/IntegrationTest.php
@@ -1,17 +1,24 @@
 <?php
 
-namespace FFraenz\PrivateComposerInstaller\Test;
+declare(strict_types=1);
 
-use Dotenv\Parser\Parser;
+namespace rcknr\ComposerAuthDotenv\Test;
+
+use donatj\MockWebServer\MockWebServer;
+use donatj\MockWebServer\Response;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Process\Process;
 
-use function class_exists;
-use function copy;
+use function base64_encode;
 use function file_get_contents;
+use function file_put_contents;
+use function join;
+use function json_encode;
 use function mkdir;
-use function preg_match;
+use function str_replace;
 use function sys_get_temp_dir;
+
+use const PHP_EOL;
 
 class IntegrationTest extends TestCase
 {
@@ -21,10 +28,16 @@ class IntegrationTest extends TestCase
     protected function setUp(): void
     {
         // Choose tmp working directory to run test case in
-        $this->pwd = sys_get_temp_dir() . '/private-composer-installer';
+        $this->pwd = sys_get_temp_dir() . '/composer-auth-dotenv';
 
         // Create working directory
         @mkdir($this->pwd);
+
+        $this->server = new MockWebServer();
+        $this->server->start();
+        $this->server->setDefaultResponse(
+            new Response('{}', ['Content-Type' => 'application/json'])
+        );
     }
 
     protected function tearDown(): void
@@ -33,57 +46,61 @@ class IntegrationTest extends TestCase
         $process = new Process(['rm', '-r', $this->pwd]);
         $process->mustRun();
         $this->pwd = null;
+        $this->server->stop();
     }
 
     public function testWordPressComposerIntegration()
     {
-        // Using a different phpdotenv version in the integration test than
-        // currently installed leads to CI issues
-        $path = class_exists(Parser::class)
-            ? __DIR__ . '/../stubs/wp-phpdotenv-5.2'
-            : __DIR__ . '/../stubs/wp-phpdotenv-4.1';
+        $repositoryHost = "{$this->server->getHost()}:{$this->server->getPort()}";
+        $credentials    = [
+            'username' => 'username',
+            'password' => 'password',
+        ];
+        $composerAuth   = json_encode([
+            'http-basic' => [
+                $repositoryHost => $credentials,
+            ],
+        ]);
 
-        $pluginFilePath = $this->pwd
-            . '/public/content/plugins/classic-editor/classic-editor.php';
+        file_put_contents(
+            $this->pwd . '/.env',
+            "COMPOSER_AUTH={$composerAuth}" . PHP_EOL
+        );
+        $composerJson = file_get_contents(__DIR__ . '/../stubs/composer-test.json');
+        file_put_contents(
+            $this->pwd . '/composer.json',
+            str_replace('{TEST_URL}', "http://{$repositoryHost}", $composerJson)
+        );
 
-        // Install project
-        copy($path . '/composer-install.json', $this->pwd . '/composer.json');
-        copy($path . '/.env', $this->pwd . '/.env');
         $install = new Process(
             [
                 __DIR__ . '/../../vendor/composer/composer/bin/composer',
                 'install',
                 '--no-interaction',
             ],
-            $this->pwd
+            $this->pwd,
         );
         $install->setTimeout(60);
         $install->mustRun();
+
         $this->assertTrue($install->isSuccessful());
 
-        // Verify plugin file
-        $pluginFile = @file_get_contents($pluginFilePath);
-        $this->assertTrue($pluginFile !== false);
-        $this->assertTrue(preg_match('/Version:\s+1\.5/', $pluginFile) === 1);
-
-        // Update phpdotenv and dependency
-        copy($path . '/composer-update.json', $this->pwd . '/composer.json');
         $update = new Process(
             [
                 __DIR__ . '/../../vendor/composer/composer/bin/composer',
                 'update',
                 '--no-interaction',
             ],
-            $this->pwd
+            $this->pwd,
         );
         $update->setTimeout(60);
         $update->mustRun();
         $this->assertTrue($update->isSuccessful());
 
-        // Verify plugin file
-        $this->assertTrue($install->isSuccessful());
-        $pluginFile = @file_get_contents($pluginFilePath);
-        $this->assertTrue($pluginFile !== false);
-        $this->assertTrue(preg_match('/Version:\s+1\.6/', $pluginFile) === 1);
+        $this->assertArrayHasKey('Authorization', $this->server->getLastRequest()?->getHeaders());
+        $this->assertSame(
+            'Basic ' . base64_encode(join(':', $credentials)),
+            $this->server->getLastRequest()->getHeaders()['Authorization']
+        );
     }
 }
